4369acc75b19c56fd8883659b2dd91107894e985
Leonardo Spuldar <spdleonardo@hotmail.com>
2020-04-29 07:30:51 -0300

    Initial commit

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,50 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        balance.income += transaction.value;
+      } else if (transaction.type === 'outcome') {
+        balance.outcome += transaction.value;
+      }
+    });
+    balance.total = balance.income - balance.outcome;
+    return balance;
+  }
+
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,28 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface RequestDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, type, value }: RequestDTO): Transaction {
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      if (balance.total - value < 0) {
+        throw Error('Your balance is not enough for this operation.');
+      }
+    }
+    return this.transactionsRepository.create({ title, type, value });
+  }
+}
+
+export default CreateTransactionService;
